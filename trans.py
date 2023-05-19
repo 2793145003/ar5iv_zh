51c64d603fdd680a6a89b55ca073e6174143d6bf
LvTianlei <24595900+2793145003@users.noreply.github.com>
2023-05-19 13:23:03 +0800

    调整了翻译格式

--- trans.py
+++ trans.py
@@ -4,39 +4,69 @@ from deep_translator import GoogleTranslator as Translator
 import re
 
 def translate_text(text):
-    print(text)
-    translator = Translator(source='en', target='zh-CN')
-    translation = translator.translate(text)
-    print(translation)
-    # time.sleep(3)
+    while True:
+        try:
+            print(text)
+            translator = Translator(source='en', target='zh-CN')
+            translation = translator.translate(text)
+            print(translation)
+            break
+        except Exception as e:
+            print(f"Translation error: {e}")
+            time.sleep(30)
+            continue
     return translation
 
-def replace_math_content(string):
-    pattern = r'(<math\b[^>]*>.*?<\/math>)'
-    count = 1
-    def repl(match):
-        nonlocal count
-        replaced_content = '【公式{}】'.format(count)
-        count += 1
-        return replaced_content
-
-    replaced_string, num_replacements = re.subn(pattern, repl, string, flags=re.DOTALL)
-    replaced_content = re.findall(pattern, string, flags=re.DOTALL)
+def replace_tags(html):
+    replaced_text = []
+    while True:
+        html = str(html)
+        html = BeautifulSoup(html, 'html.parser')
+        con = False
+        for tag in html.find_all(True):
+            if (tag.string and '【占' not in tag.string) or tag.name == 'math':
+                replaced_text.append(str(tag))
+                tag.replace_with(f'【占{len(replaced_text)-1}】')
+                con = True
+                break
+        if not con:
+            break
     
-    return replaced_string, replaced_content
+    replaced_html = str(html)
+    return replaced_html, replaced_text
 
-def restore_math_content(string, replacements):
-    pattern = r'【公式\d+】'
+def trans_tags(tags):
+    results = []
+    for tag in tags:
+        tag = BeautifulSoup(tag, 'html.parser')
+        if tag.name == 'math':
+            results.append(str(tag))
+            continue
+        if not tag.string:
+            results.append(str(tag))
+            continue
+        s = tag.text
+        if ' ' not in s:
+            results.append(str(tag))
+            continue
+        trans = translate_text(s)
+        tag.string.replace_with(trans)
+        results.append(str(tag))
+    return results
 
-    def restore(match):
-        replacement = replacements[restore.counter]
-        restore.counter += 1
-        return replacement
+def remove_spaces_between_brackets(text):
+    # 移除'【'与'】'之间的所有空格
+    pattern = r'【\s*([^【】]+)\s*】'
+    repl = lambda match: '【' + match.group(1).replace(' ', '') + '】'
+    result = re.sub(pattern, repl, text)
+    return result
 
-    restore.counter = 0
-
-    restored_string = re.sub(pattern, restore, string)
-    return restored_string
+def add_tags(paragraph, tags):
+    paragraph = str(paragraph)
+    paragraph = remove_spaces_between_brackets(paragraph)
+    for i in range(len(tags)):
+        paragraph = paragraph.replace(f'【占{i}】', tags[i])
+    return paragraph
 
 def translate_html(input_path, output_path):
     with open(input_path, 'r') as f:
@@ -47,19 +77,21 @@ def translate_html(input_path, output_path):
     paragraphs = soup.find_all('p')
 
     for paragraph in paragraphs:
-        # original_text = paragraph.get_text()
-        no_math_paragraph, math_contents = replace_math_content(str(paragraph))
-        original_text = no_math_paragraph#.get_text()
+        pure, tags = replace_tags(paragraph)
+        pure = (BeautifulSoup(pure, 'html.parser'))
+        original_text = pure.get_text()
         text = translate_text(original_text)
-        translated_text = restore_math_content(text, math_contents)
-        paragraph.insert_after(BeautifulSoup(f'{translated_text}', 'html.parser'))
+        pure.string = text
+        tags = trans_tags(tags)
+        pure = add_tags(pure, tags)
+        paragraph.insert_after(BeautifulSoup(pure, 'html.parser'))
 
     with open(output_path, 'w') as f:
         f.write(str(soup))
 
 
-
-# 示例用法
-input_html = "html/main.html"
-output_html = "html/paper.html"
-translate_html(input_html, output_html)
+if __name__ == "__main__": 
+    # 示例用法
+    input_html = "html/main.html"
+    output_html = "html/paper.html"
+    translate_html(input_html, output_html)
